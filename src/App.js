0f8ec18f1323f5b2e367cf305faef742f354a2da
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-26 13:29:23 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import data from './data'
+import './index.css';
+import Article from './Article';
+const getTheme=()=>{
+ /*  let curTheme = localStorage.getItem("theme")
+  if (curTheme) {
+    return curTheme
+  }else{
+    return 'light-theme'
+  } */
 
-import './App.css';
-
+  let theme='light-theme'
+  if (localStorage.getItem("theme")) {
+   return theme=localStorage.getItem("theme")
+  }
+  return theme
+}
 function App() {
+  const [theme, setTheme] = useState(getTheme());
+  useEffect(() => {
+   document.documentElement.classList=theme
+   localStorage.setItem("theme",theme)
+  }, [theme]);
+  const toggleTheme = ()=>{
+    setTheme(theme==='light-theme'?'dark-theme': 'light-theme')
+  }
   return (
-    <div className="App">
-      
-    </div>
+    <main >
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
   );
 }
 
